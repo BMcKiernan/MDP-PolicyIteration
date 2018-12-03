3375f7d1bdf84f0c10cf6c4e10eda923d47c30d1
JetiAC <https://JetiAC@bitbucket.org>
2018-12-02 23:31:26 -0500

    old

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ discount = 0.9
 total_rows = 3
 total_cols = 4
 
-
+old_state_space = None
 
 #actions are ((.1 prob of going left), (.9 prob of going desired direction))
 actions = {'right':('up', 'right'), 'up':('left', 'up'), 'left':('down', 'left'), 'down':('right','down'),}
@@ -70,32 +70,32 @@ def transition_iteration():
 #action is any of the actions dictionary  
 def get_position_value(state, action):
     val = -99
-    row_max = len(state_space) -1 
-    col_max = len(state_space[0]) -1 
+    row_max = len(old_state_space) -1 
+    col_max = len(old_state_space[0]) -1 
     if action == 'up':
         # in first row can't go up || there is obstacle
-        if state[0] == 0 or state_space[state[0] - 1][state[1]] is None:
-            val = state_space[state[0]][state[1]] 
+        if state[0] == 0 or old_state_space[state[0] - 1][state[1]] is None:
+            val = old_state_space[state[0]][state[1]] 
         else:
-            val = state_space[state[0] - 1][state[1]]
+            val = old_state_space[state[0] - 1][state[1]]
     if action == 'down':
         # in last row can't go down || there is obstacle
-        if state[0] == row_max or state_space[state[0] + 1][state[1]] is None:
-            val = state_space[state[0]][state[1]]
+        if state[0] == row_max or old_state_space[state[0] + 1][state[1]] is None:
+            val = old_state_space[state[0]][state[1]]
         else:
-            val = state_space[state[0] + 1][state[1]]
+            val = old_state_space[state[0] + 1][state[1]]
     if action == 'left':
         # in first column can't go left || there is obstacle
-        if state[1] == 0 or state_space[state[0]][state[1] - 1] is None: 
-            val = state_space[state[0]][state[1]]
+        if state[1] == 0 or old_state_space[state[0]][state[1] - 1] is None: 
+            val = old_state_space[state[0]][state[1]]
         else:
-            val = state_space[state[0]][state[1] - 1]
+            val = old_state_space[state[0]][state[1] - 1]
     if action == 'right':
         # in last column can't go right || there is obstacle
-        if state[1] == col_max or state_space[state[0]][state[1] + 1] is None:
-            val = state_space[state[0]][state[1]]
+        if state[1] == col_max or old_state_space[state[0]][state[1] + 1] is None:
+            val = old_state_space[state[0]][state[1]]
         else: 
-            val = state_space[state[0]][state[1] + 1]
+            val = old_state_space[state[0]][state[1] + 1]
     if val == -99:
          print "\n In get_position_value \n life is pain \n"
     return val
@@ -122,6 +122,11 @@ if __name__ == "__main__":
     policy_space[0][3] = 1
     policy_space[1][3] = -1
 
+    old_state_space = [[]] * total_cols
+    for i, v in enumerate(state_space):
+    	for e in v:
+    		old_state_space[i].append(e)
+
     print "Printing setup" 
     print_space(state_space, "state_space", obstacle)
     print_space(policy_space, "policy", obstacle)
@@ -130,6 +135,9 @@ if __name__ == "__main__":
     p = 12
     while(i < p):
         transition_iteration()
+        for j, row in enumerate(state_space):
+        	for k, e in enumerate(row):
+        		old_state_space[j][k] = e
         i += 1
         print "After " + str(i) +  " iterations" 
         print_space(state_space, "state_space", obstacle)
